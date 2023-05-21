e5a28a9bb99174127c8eb6401e43b7d2c2f2d074
mberrish <mikheil.berishvili12@gmail.com>
2023-05-21 22:40:48 +0100

    cleanup and fix bugs

--- app/src/main/java/com/example/tictactoe/AuthActivity.kt
+++ app/src/main/java/com/example/tictactoe/AuthActivity.kt
@@ -5,17 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
-import androidx.lifecycle.lifecycleScope
 import com.example.tictactoe.Database.AppDatabase
 import com.example.tictactoe.Database.Users.User
 import com.example.tictactoe.Database.Users.UserRepository
-import kotlinx.coroutines.launch
 
 class AuthActivity : AppCompatActivity() {
-    lateinit var userRepository: UserRepository
-        private set
-
+    private lateinit var userRepository: UserRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,55 +19,36 @@ class AuthActivity : AppCompatActivity() {
         val userDao = AppDatabase.getInstance(applicationContext).userDao()
         userRepository = UserRepository(userDao)
 
+        val isInternalUserExist = userRepository.checkIfInternalUserExist()
 
-        //userRepository.getUsers()
-
-      //  val mainViewModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(
-       //     MainViewModel::class.java)
-
-        lifecycleScope.launch {
-            val isInternalUserExist = userRepository.checkIfInternalUserExist()
-
-            if (isInternalUserExist) {
-                val name = userRepository.getInternalUserName()
-                redirectToHomePage(name)
-            } else {
-                setContentView(R.layout.activity_auth)
-                val nextButton = findViewById<Button>(R.id.nextButton)
-                nextButton.setOnClickListener {
-                    val nameEditText = findViewById<EditText>(R.id.nameEditText);
-                    val name = nameEditText.text.toString()
-                    createInternalUser(name);
-                    redirectToHomePage(name);
-                }
+        if (isInternalUserExist) {
+            val name = userRepository.getInternalUserName()
+            redirectToHomePage(name)
+        } else {
+            setContentView(R.layout.activity_auth)
+            val nextButton = findViewById<Button>(R.id.nextButton)
+            nextButton.setOnClickListener {
+                val nameEditText = findViewById<EditText>(R.id.nameEditText);
+                val name = nameEditText.text.toString()
+                createInternalUser(name);
+                redirectToHomePage(name);
             }
-
-
         }
-
-        Toast.makeText(this, "Hello!", Toast.LENGTH_SHORT).show()
-
-        //val nextButton = findViewById< Button>(R.id.nextButton)
-        //val binding = ActivityDetailBinding.inflate(layoutInflater)
-
-        //setContentView(binding.root)
-
     }
 
-    fun redirectToHomePage(name:String){
+    private fun redirectToHomePage(name:String){
         val intent = Intent(this, HomePageActivity::class.java)
         intent.putExtra("name", name)
         startActivity(intent)
     }
 
-    fun createInternalUser(name:String)
+    private fun createInternalUser(name:String)
     {
         val newUser = User(
             userName = name,
             isExternal = false
         )
-        lifecycleScope.launch() {
-            userRepository.insertUser(newUser);
-        }
+
+        userRepository.insertUser(newUser);
     }
 }
